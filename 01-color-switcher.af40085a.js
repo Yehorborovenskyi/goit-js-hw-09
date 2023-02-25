88ec1b9b911147fe4085a04492e3b9dd32b7f279
Yehorborovenskyi <100347969+Yehorborovenskyi@users.noreply.github.com>
2023-02-25 16:06:51 +0000

    Deploying to gh-pages from @ Yehorborovenskyi/goit-js-hw-09@d422389b91937778e5f6a8020413792ed4a3941a 🚀

---
+++ 01-color-switcher.af40085a.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");d.disabled=!0,timerId=null,t.addEventListener("click",(function(){timerId=setInterval((()=>{t.disabled=!0,d.disabled=!1,e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),d.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1,d.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.af40085a.js.map
