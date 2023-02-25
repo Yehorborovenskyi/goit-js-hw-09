26204c426b0794821642165911269e2d7c2fa541
Yehorborovenskyi <yehorborovenskyi@gmail.com>
2023-02-25 13:24:16 +0300

    task-01

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,13 @@
+import '../css/common.css';
+
+// const logger = time => {
+//   console.log('Лог через ${time}ms, потому что не отменили таймаут');
+// };
+// const timerId = setTimeout(logger, 2000, 2000);
+// console.log(timerId);
+
+// const shouldCancelTimer = Math.random() > 0.4;
+// console.log(shouldCancelTimer);
+// if (shouldCancelTimer) {
+//   clearTimeout(timerId);
+// }
